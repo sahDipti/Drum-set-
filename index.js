1294601ac5615b5918f6263927cc1f72d050b931
sahDipti <123860643+sahDipti@users.noreply.github.com>
2023-07-04 23:41:42 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -4,29 +4,48 @@ var numberOfButtons=document.querySelectorAll(".drum").length;
 for(var i=0;i<numberOfButtons;i++){
     document.querySelectorAll("button")[i].addEventListener("click",function (){
         var buttonClicked=this.innerHTML;
-        var audio;
-        switch(buttonClicked){
-            case 'w':
-                audio=new Audio("sounds/crash.mp3");
-                break;
-            case 'a':
-                audio=new Audio("sounds/kick-bass.mp3");
-                break;
-            case 's':
-                audio=new Audio("sounds/snare.mp3");
-                break;
-            case 'd':
-                audio=new Audio("sounds/tom-4.mp3");
-                break;
-            case 'j':
-                audio=new Audio("sounds/tom-3.mp3");
-                break;
-            case 'k':
-                audio=new Audio("sounds/tom-2.mp3");
-                break;
-            default: 
-                audio=new Audio("sounds/tom-1.mp3");
-        }
-        audio.play();
+        playSound(buttonClicked);
     });
 }
+
+document.addEventListener("keydown",function(event){
+    var keyVal=event.key;
+    playSound(keyVal);
+})
+
+function playSound(key){
+    var audio;
+        switch(key){
+            case 'w':
+                var audio=new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case 'a':
+                var audio=new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case 's':
+                var audio=new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case 'd':
+                var audio=new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case 'j':
+                var audio=new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case 'k':
+                var audio=new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case 'l':
+                var audio=new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            default: 
+        }
+}
+
+
